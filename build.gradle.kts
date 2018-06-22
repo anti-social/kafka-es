ba2eb2d460c5c4270ec145ff6a74c7351601f153
Alexander Koval <kovalidis@gmail.com>
2018-06-22 19:59:13 +0300

    Uploading artifact on bintray

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,29 @@
 import com.google.protobuf.gradle.ExecutableLocator
-import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
+import com.jfrog.bintray.gradle.BintrayExtension
 import java.nio.file.Paths
+import java.util.Date
+import org.gradle.jvm.tasks.Jar
+import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     application
     java
+    `maven-publish`
     id("org.jetbrains.kotlin.jvm") version "1.2.50"
     id("com.google.protobuf") version "0.8.5"
+    id("com.jfrog.bintray") version "1.8.2"
+    id("org.ajoberstar.grgit") version "2.2.1"
 }
 
 repositories {
      mavenCentral()
 }
+
+val grgit: org.ajoberstar.grgit.Grgit by extra
+val gitDescribe = grgit.describe(mapOf("match" to listOf("v*")))
+        ?: "v0.0.0-unknown"
+version = gitDescribe.trimStart('v')
 
 val kafkaVersion = "1.0.1"
 val jestVersion = "5.3.3"
@@ -74,4 +85,40 @@ val compileKotlin by tasks.getting(KotlinCompile::class) {
 }
 val compileTestKotlin by tasks.getting(KotlinCompile::class) {
     dependsOn("generateTestProto")
+}
+
+val jar by tasks.getting(Jar::class)
+
+publishing {
+    (publications) {
+        "jar"(MavenPublication::class) {
+            groupId = "company.evo"
+            artifactId = project.name
+            version = project.version.toString()
+            from(components["java"])
+        }
+    }
+}
+
+bintray {
+    user = properties["bintrayUser"]?.toString()
+            ?: System.getenv("BINTRAY_USER")
+    key = properties["bintrayApiKey"]?.toString()
+            ?: System.getenv("BINTRAY_API_KEY")
+    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
+        repo = "maven"
+        name = project.name
+        userOrg = "evo"
+        setLicenses("Apache-2.0")
+        setLabels("kafka-connect", "elasticsearch-connector", "kafka-elasticsearch-sink")
+        vcsUrl = "https://github.com/anti-social/kafka-es.git"
+        version(delegateClosureOf<BintrayExtension.VersionConfig> {
+            name = "kafka-es"
+            released = Date().toString()
+            vcsTag = gitDescribe
+        })
+    })
+    setPublications("jar")
+    publish = true
+    dryRun = hasProperty("bintrayDryRun")
 }
